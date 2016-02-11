c0a3326d9394aae296eab3eeba01c9cd2e1555ed
Paul Hooper <phooper0001@yahoo.com>
2016-02-11 22:41:10 +0000

    Testing ....

--- DeleteNexusRepos.sh
+++ DeleteNexusRepos.sh
@@ -5,14 +5,14 @@ if ! $(wget -q http://aretzm01:8081/nexus/ ) ; then
 fi
 
 
-curl --request DELETE --write "%{http_code} %{url_effective}\\n" --user admin:admin123 --output /dev/null --silent http://aretzm01:8081/nexus/content/repositories/snapshots/uk/ >/dev/null 2>&1
+#curl --request DELETE --write "%{http_code} %{url_effective}\\n" --user admin:admin123 --output /dev/null --silent http://aretzm01:8081/nexus/content/repositories/snapshots/uk/ >/dev/null 2>&1
 
-#curl --request DELETE --user admin:admin123 --output /dev/null --silent http://aretzm01:8081/nexus/content/repositories/snapshots/uk/ >/dev/null 2>&1
+curl --request DELETE --user admin:admin123 --output /dev/null --silent http://aretzm01:8081/nexus/content/repositories/snapshots/uk/ >/dev/null 2>&1
 
 
-curl --request DELETE --write "%{http_code} %{url_effective}\\n" --user admin:admin123 --output /dev/null --silent http://aretzm01:8081/nexus/content/repositories/releases/uk/ >/dev/null 2>&1
+#curl --request DELETE --write "%{http_code} %{url_effective}\\n" --user admin:admin123 --output /dev/null --silent http://aretzm01:8081/nexus/content/repositories/releases/uk/ >/dev/null 2>&1
 
-#curl --request DELETE --user admin:admin123 --output /dev/null --silent http://aretzm01:8081/nexus/content/repositories/releases/uk/ >/dev/null 2>&1
+curl --request DELETE --user admin:admin123 --output /dev/null --silent http://aretzm01:8081/nexus/content/repositories/releases/uk/ >/dev/null 2>&1
 
 
 
--- DeployAndRelease.sh
+++ DeployAndRelease.sh
@@ -1,23 +1,32 @@
 
 CurrentVersion=$(grep "<version>.*SNAPSHOT</version>" Function/pom.xml| sed -e 's!<version>!!g' -e 's!</version>!!g' | tr -d " ")
+CurrentVersion=$(echo ${CurrentVersion//[[:blank:]]/})
 CurrentVersionWithoutSnapshot=$(echo $CurrentVersion  | sed -e 's!-SNAPSHOT!!g')
 
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
-if [ -x $SCRIPT_DIR/DeleteNexusRepos.sh ] ; then
-    $SCRIPT_DIR/DeleteNexusRepos.sh
-else
-    echo "No delete script present [ $SCRIPT_DIR/DeleteNexusRepos.sh ]. Exiting"
-    exit 1
-fi
+#if [ -x $SCRIPT_DIR/DeleteNexusRepos.sh ] ; then
+    #$SCRIPT_DIR/DeleteNexusRepos.sh
+#else
+    #echo "No delete script present [ $SCRIPT_DIR/DeleteNexusRepos.sh ]. Exiting"
+    #exit 1
+#fi
 
 # Upload the SNAPSHOT version of the war and rpm to nexus - this is the current version as it's checked out of GIT 
-mvn -q -f Function/pom.xml deploy
-mvn -q -f ServiceDescription/pom.xml versions:set -DnewVersion=$CurrentVersion -Dmvn_version=$CurrentVersion
-mvn -q -f ServiceDescription/pom.xml -Dmvn_version=$CurrentVersion deploy
+echo "mvn -q -DskipTests -f Function/pom.xml clean deploy"
+mvn -DskipTests -f Function/pom.xml clean deploy || { echo "Function/pom.xml failed" ; exit 1 ; } 
+
+if [ -d ServiceDescription ] ; then 
+    echo "mvn -q -f ServiceDescription/pom.xml versions:set -DnewVersion=$CurrentVersion -Dmvn_version=$CurrentVersion"
+    mvn -DskipTests -f ServiceDescription/pom.xml versions:set -DnewVersion=$CurrentVersion -Dmvn_version=$CurrentVersion || { echo "ServiceDescription/pom.xml failed" ; exit 1 ; } 
+
+    echo "mvn -q -DskipTests -f ServiceDescription/pom.xml -Dmvn_version=$CurrentVersion clean deploy"
+    mvn -DskipTests -f ServiceDescription/pom.xml -Dmvn_version=$CurrentVersion clean deploy || { echo "ServiceDescription/pom.xml failed" ; exit 1 ; }
+fi
 
 # Change the version number in the POM to remove the snapshot identifier and RELEASE into NEXUS
-mvn -q -f Function/pom.xml versions:set -DnewVersion=$CurrentVersionWithoutSnapshot -Dmvn_version=$CurrentVersionWithoutSnapshot 
-mvn -q -f Function/pom.xml deploy
-mvn -q -f ServiceDescription/pom.xml versions:set -DnewVersion=$CurrentVersionWithoutSnapshot -Dmvn_version=$CurrentVersionWithoutSnapshot
-mvn -q -f ServiceDescription/pom.xml -Dmvn_version=$CurrentVersionWithoutSnapshot deploy
+# This should only happen after the build has been successfully tested 
+#mvn -q -f Function/pom.xml versions:set -DnewVersion=$CurrentVersionWithoutSnapshot -Dmvn_version=$CurrentVersionWithoutSnapshot 
+#mvn -q -f Function/pom.xml deploy
+#mvn -q -f ServiceDescription/pom.xml versions:set -DnewVersion=$CurrentVersionWithoutSnapshot -Dmvn_version=$CurrentVersionWithoutSnapshot
+#mvn -q -f ServiceDescription/pom.xml -Dmvn_version=$CurrentVersionWithoutSnapshot deploy
